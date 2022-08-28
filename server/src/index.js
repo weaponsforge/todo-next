f5939560a0e6416cd956badba69a3bcfa66366d3
weaponsforge <forgeweapons@gmail.com>
2022-08-29 00:23:46 +0800

    feat: Use an api rate limiter, copied from /weaponsforge/apidocs-swagger-ui

--- server/src/index.js
+++ server/src/index.js
@@ -4,11 +4,18 @@ require('./utils/db')
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const rateLimit = require('express-rate-limit')
 const app = express()
 const PORT = process.env.PORT || 3001
 
 const { corsOptions } = require('./utils/cors_options')
 const controllers = require('./controllers')
+
+const limiter = rateLimit({
+  windowMs: process.env.API_WINDOW_MS_MINUTES * 60 * 1000, // in minutes
+  max: process.env.API_RATE_LIMIT, // limit each IP to API_RATE_LIMIT requests per windowMs
+  message: 'Too many requests from this IP. Please try again after 15 minutes.'
+})
 
 // Initialize the express app
 app.use(express.json())
@@ -19,7 +26,7 @@ if (process.env.ALLOW_CORS === '1') {
   app.use(cors(corsOptions))
 }
 
-app.use('/api', controllers)
+app.use('/api', limiter, controllers)
 
 app.get('*', (req, res) => {
   return res.status(200).send('Welcome to the Todo API')
